14bc122e146288c3074116f528e3f8a26b7654b3
haoliang <haoliang0.1.2@gmail.com>
2023-08-11 22:20:20 +0800

    sync with upstream

--- lua/nag.lua
+++ lua/nag.lua
@@ -7,7 +7,8 @@
 -- * no nested nag
 -- * only one nag for one buffer
 -- * hard to properly maintain the original buffer's &modifiable, so let user concern it
--- * no syntax/filetype nor treesitter/lsp support at now
+-- * no syntax/filetype nor treesitter/lsp support
+-- * diff+patch
 --
 
 local M = {}
@@ -15,6 +16,7 @@ local M = {}
 local api = vim.api
 
 local bufrename = require("infra.bufrename")
+local ctx = require("infra.ctx")
 local Ephemeral = require("infra.Ephemeral")
 local ex = require("infra.ex")
 local jelly = require("infra.jellyfish")("nag")
@@ -26,37 +28,98 @@ local launch
 do
   local function make_nag_name(bufname, start, stop) return string.format("nag://%s@%s~%s", vim.fn.fnamemodify(bufname, ":t"), start, stop) end
 
-  local function is_valid_nag_buf(bufname) return strlib.startswith(bufname, "nag://") end
+  local function is_nag_buf(bufname) return strlib.startswith(bufname, "nag://") end
 
-  function launch(src_win_id, src_bufnr, edit_cmd)
-    assert(src_win_id and src_bufnr and edit_cmd)
+  ---@param bufnr integer
+  ---@param start integer
+  ---@param stop integer
+  ---@return string
+  local function join_lines(bufnr, start, stop)
+    local lines = api.nvim_buf_get_lines(bufnr, start, stop, false)
+    table.insert(lines, "\n")
+    return table.concat(lines, "\n")
+  end
 
-    local origin_state, selines
+  ---start: lnum, 0-based, inclusive
+  ---stop: lnum, 0-based, exclusive
+  ---@param host nag.Host
+  ---@param nag_bufnr integer
+  local function diffpatch(host, nag_bufnr)
+    local hunks
     do
-      local bufname = api.nvim_buf_get_name(src_bufnr)
-      if is_valid_nag_buf(bufname) then return jelly.warn("nag on nag") end
-
-      local range = vsel.range(src_bufnr)
-      if range == nil then return jelly.warn("no selection") end
-
-      origin_state = {
-        winid = src_win_id,
-        bufnr = src_bufnr,
-        name = bufname,
-        start_line = range.start_line,
-        stop_line = range.stop_line,
-      }
-
-      selines = vsel.multiline_text(origin_state.bufnr)
-      if selines == nil then return end
+      local a = join_lines(host.bufnr, host.start, host.stop)
+      local b = join_lines(nag_bufnr, 0, -1)
+      hunks = vim.diff(a, b, { result_type = "indices" })
+      if #hunks == 0 then return jelly.debug("no changes") end
     end
+    local offset = host.start
+    for i = #hunks, 1, -1 do
+      local start_a, count_a, start_b, count_b = unpack(hunks[i])
 
-    local mux = sync.create_buf_mutex(origin_state.bufnr, "nag")
+      local lines
+      if count_b == 0 then
+        lines = {}
+      else
+        local start = start_b - 1
+        lines = api.nvim_buf_get_lines(nag_bufnr, start, start + count_b, false)
+      end
+
+      do
+        local start, stop
+        if count_a == 0 then -- append
+          start = start_a - 1 + offset + 1
+          stop = start
+        elseif count_b == 0 then -- delete
+          start = start_a - 1 + offset
+          stop = start + count_a
+        else
+          start = start_a - 1 + offset
+          stop = start + count_a
+        end
+        api.nvim_buf_set_lines(host.bufnr, start, stop, false, lines)
+      end
+    end
+  end
+
+  ---@class nag.Host
+  ---@field winid integer
+  ---@field bufnr integer
+  ---@field name string
+  ---@field start integer
+  ---@field stop integer
+
+  ---@return nag.Host?
+  local function Host(winid)
+    local bufnr = api.nvim_win_get_buf(winid)
+    local bufname = api.nvim_buf_get_name(bufnr)
+    if is_nag_buf(bufname) then return jelly.warn("nag on nag") end
+
+    local range = vsel.range(bufnr)
+    if range == nil then return jelly.warn("no selection") end
+
+    return {
+      winid = winid,
+      bufnr = bufnr,
+      name = bufname,
+      start = range.start_line,
+      stop = range.stop_line,
+    }
+  end
+
+  ---@param host_winid integer
+  ---@param edit_cmd string @sp,vs,tabe ..., but no modifiers
+  function launch(host_winid, edit_cmd)
+    assert(host_winid and edit_cmd)
+
+    local host = Host(host_winid)
+    if host == nil then return end
+
+    local mux = sync.create_buf_mutex(host.bufnr, "nag")
     if not mux:acquire() then return jelly.warn("nag is running already") end
 
     local nag_bufnr
-    do -- setup nag buf
-      nag_bufnr = Ephemeral({ modifiable = true }, selines)
+    do
+      nag_bufnr = Ephemeral({ modifiable = true }, api.nvim_buf_get_lines(host.bufnr, host.start, host.stop, false))
       local tick0 = api.nvim_buf_get_changedtick(nag_bufnr)
 
       api.nvim_create_autocmd("bufwipeout", {
@@ -64,40 +127,32 @@ do
         once = true,
         callback = function()
           mux:release()
-          if not api.nvim_buf_is_valid(origin_state.bufnr) then return jelly.warn("original buf#%d was gone", origin_state.bufnr) end
+          if not api.nvim_buf_is_valid(host.bufnr) then return jelly.warn("original buf#%d was gone", host.bufnr) end
           --determining &modified base on changedtick is not accurate, yet it's still good enough.
           if api.nvim_buf_get_changedtick(nag_bufnr) == tick0 then return jelly.debug("nag buf has not been modified") end
-          local ok, err = xpcall(function()
-            local lines = api.nvim_buf_get_lines(nag_bufnr, 0, -1, true)
-            api.nvim_buf_set_lines(origin_state.bufnr, origin_state.start_line, origin_state.stop_line, false, lines)
-          end, debug.traceback)
-          if not ok then jelly.error(err) end
+          ctx.undoblock(host.bufnr, function() diffpatch(host, nag_bufnr) end)
         end,
       })
     end
 
-    local nag_bufname
+    local nag_name
     do
-      nag_bufname = make_nag_name(origin_state.name, origin_state.start_line, origin_state.stop_line)
-      bufrename(nag_bufnr, nag_bufname)
+      nag_name = make_nag_name(host.name, host.start, host.stop)
+      bufrename(nag_bufnr, nag_name)
     end
 
-    do -- setup nag window
-      ex(edit_cmd, nag_bufname)
+    do
+      ex(edit_cmd, nag_name)
       local nag_winid = api.nvim_get_current_win()
-      assert(nag_winid ~= origin_state.winid)
-      api.nvim_win_set_buf(nag_winid, nag_bufnr)
+      assert(nag_winid ~= host.winid)
+      assert(api.nvim_win_get_buf(nag_winid) == nag_bufnr)
     end
   end
 end
 
 do
   local function launcher(edit_cmd)
-    return function()
-      local winid = api.nvim_get_current_win()
-      local bufnr = api.nvim_win_get_buf(winid)
-      launch(winid, bufnr, edit_cmd)
-    end
+    return function() launch(api.nvim_get_current_win(), edit_cmd) end
   end
 
   M.tab = launcher("tabe")
